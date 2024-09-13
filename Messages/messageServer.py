bfdd449141b2c31856d6e10953ab87c411d77ec7
DoubtingFive <135340856+DoubtingFive@users.noreply.github.com>
2024-09-13 19:26:01 +0200

    Update messageServer.py

--- Messages/messageServer.py
+++ Messages/messageServer.py
@@ -12,7 +12,7 @@ def AcceptConnection():
 def Connect(c, a):
     u = c.recv(16).decode()
     newConnectionMessage = f"{colorama.Fore.LIGHTYELLOW_EX} >>> Get connection from {a[0]} as {u}."
-    print(newConnectionMessage) # check port command
+    print(newConnectionMessage)
     newConnectionMessage = newConnectionMessage.encode()
     c.send(chat.encode())
     for x in conn_list:
@@ -31,7 +31,6 @@ def HandleConnection(c,a,u):
             for x in conn_list:
                 if x == c: continue
                 x.send(recv)
-
         except Exception as e:
             if c in conn_list:
                 conn_list.remove(c)
@@ -39,6 +38,7 @@ def HandleConnection(c,a,u):
             lostConnectionMessage = f"{colorama.Fore.RED} <<< Lost connection from {a} as {u}.{colorama.Fore.RESET}".encode()
             for x in conn_list:
                 x.send(lostConnectionMessage)
+            break
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn_list = []
