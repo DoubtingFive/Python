139e80222480e9aef4816876f1ef939f0e1aabb5
Az0x <135340856+DoubtingFive@users.noreply.github.com>
2024-10-04 11:15:08 +0200

    Update block.py

--- Pygame/block.py
+++ Pygame/block.py
@@ -2,9 +2,12 @@ from pygame import *
 import random
 
 init()
-screen = display.set_mode((1280, 720))
-screenX = screen.get_width()
-screenY = screen.get_height()
+screenX = 1280
+screenY = 720
+screen = display.set_mode((screenX, screenY))
+
+font.init()
+text_font = font.SysFont('impact', 80)
 
 leftSide = screenX/4-50
 rightSide = (screenX/4)*3-50
@@ -17,31 +20,45 @@ colors = ("black","white","red","blue")
 isLeft = True
 rightDanger = False
 leftDanger = False
+isGame = True
+
+score = 0
 
 def Obstacle():
     global rightDanger
     global leftDanger
+    global isGame
+    global score
+    score += 1
     x = random.randint(0,3)
     if x == 0:
-        obstacles.append([leftSide,0,colors[2],0])
+        obstacles.append([leftSide,0,colors[2],0,True])
     if x == 1:
-        obstacles.append([rightSide,0,colors[3],0])
+        obstacles.append([rightSide,0,colors[3],0,False])
     obstaclesRem = []
     leftDanger = False
     rightDanger = False
     for x in range(len(obstacles)):
         obstacles[x][1] += screenY/8
         obstacles[x][3] += 1
-        if obstacles[x][3] == 6:
+        if obstacles[x][3] == 6 and obstacles[x][4]:
             leftDanger = True
-        if obstacles[x][3] == 6:
+        if obstacles[x][3] == 6 and not obstacles[x][4]:
             rightDanger = True
         if obstacles[x][3] >= 8:
             obstaclesRem.append(obstacles[x])
     for x in obstaclesRem:
         obstacles.remove(x)
 
+def Reset():
+    global obstacles
+    global score
+    score = 0
+    obstacles = []
+    Obstacle()
+
 Obstacle()
+
 
 while running:
     for e in event.get():
@@ -60,12 +77,16 @@ while running:
         draw.rect(screen,x[2],Rect(x[0],x[1]-50,50,50))
     if isLeft:
         draw.rect(screen,colors[1],Rect(leftSide,(screenY/4)*3-50,50,50))
-        if leftDanger:
-            print("Game Over")
+        if leftDanger and isGame:
+            Reset()
     else:
         draw.rect(screen,colors[1],Rect(rightSide,(screenY/4)*3-50,50,50))
-        if rightDanger:
-            print("Game Over")
+        if rightDanger and isGame:
+            Reset()
+    text_surface = text_font.render(str(score), False, colors[1])
+    textWidth = text_surface.get_width()/2
+    textHeight = text_surface.get_height()/2
+    screen.blit(text_surface, (screenX/2-textWidth,screenY/4-textHeight))
     display.flip()
 
     clock.tick(60)  # limits FPS to 60
